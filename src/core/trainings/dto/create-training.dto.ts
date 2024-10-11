5b28d18298aa5df62738b618bbc968de2cc15597
Range18 <shakmate@mail.ru>
2024-10-12 01:47:59 +0500

    Update create-training.dto.ts

--- src/core/trainings/dto/create-training.dto.ts
+++ src/core/trainings/dto/create-training.dto.ts
@@ -1,26 +1,16 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class CreateTrainingDto {
-  @ApiProperty()
   readonly slot: number;
 
-  @ApiProperty()
   readonly date: Date;
 
-  @ApiProperty({ type: [Number] })
   readonly client: number[];
 
-  @ApiProperty()
   readonly club: number;
 
-  @Transform(({ ...value }) => Number(value))
-  @IsNumber()
   readonly tariff: number;
 
-  @IsBoolean()
-  @Transform(({ ...value }) => String(value) == 'true')
   @IsOptional()
   isRepeated?: boolean;
 }
