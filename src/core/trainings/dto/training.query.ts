751a11c3d23358f68ccb639ca531eab41e538e3e
Range18 <shakmate@mail.ru>
2024-10-27 21:19:03 +0500

    Remove AuthGuard

--- src/core/trainings/dto/training.query.ts
+++ src/core/trainings/dto/training.query.ts
@@ -1,15 +1,19 @@
 import { IsDateString, IsNumber, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class TrainingQuery {
   @IsNumber()
+  @Type(() => Number)
   @IsOptional()
   clientId?: number;
 
   @IsNumber()
+  @Type(() => Number)
   @IsOptional()
   trainerId?: number;
 
   @IsNumber()
+  @Type(() => Number)
   @IsOptional()
   clubId?: number;
 
