49423ad64c68e25dbb7e36ca51aa3a8301700915
Range18 <shakmate@mail.ru>
2024-11-06 16:42:19 +0500

    Fix slots controller

--- src/core/club-slots/studio-slots.controller.ts
+++ src/core/club-slots/studio-slots.controller.ts
@@ -1,22 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { StudioSlotsService } from './studio-slots.service';
-import { CreateStudioSlotDto } from './dto/create-studio-slot.dto';
 import { AuthGuard } from '#src/common/decorators/guards/authGuard.decorator';
-import {
-  ApiBody,
-  ApiCreatedResponse,
-  ApiOkResponse,
-  ApiQuery,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { GetClubSlotRdo } from '#src/core/club-slots/rdo/get-club-slot.rdo';
 import { GetSlotsForStudio } from '#src/core/club-slots/rdo/get-slots-for-studio';
 import { GetTimeTableRdo } from '#src/core/club-slots/rdo/get-time-table.rdo';
@@ -25,26 +10,26 @@ import { GetTimeTableRdo } from '#src/core/club-slots/rdo/get-time-table.rdo';
 @Controller('api')
 export class StudioSlotsController {
   constructor(private readonly studioSlotsService: StudioSlotsService) {}
-
-  @ApiCreatedResponse({ type: GetClubSlotRdo })
-  @ApiBody({ type: CreateStudioSlotDto })
-  @AuthGuard()
-  @Post('studio/:studioId/slots')
-  async create(
-    @Param('studioId') studioId: number,
-    @Body() createClubSlotDto: CreateStudioSlotDto,
-  ) {
-    return await this.studioSlotsService.save({
-      ...createClubSlotDto,
-      studio: { id: studioId },
-    });
-  }
+  //
+  // @ApiCreatedResponse({ type: GetClubSlotRdo })
+  // @ApiBody({ type: CreateStudioSlotDto })
+  // @AuthGuard()
+  // @Post('studio/:studioId/slots')
+  // async create(
+  //   @Param('studioId') studioId: number,
+  //   @Body() createClubSlotDto: CreateStudioSlotDto,
+  // ) {
+  //   return await this.studioSlotsService.save({
+  //     ...createClubSlotDto,
+  //     studio: { id: studioId },
+  //   });
+  // }
 
   @ApiOkResponse({ type: [GetClubSlotRdo] })
   @ApiQuery({ name: 'date' })
   @Get('clubs/:clubId/slots')
   async findAllForClub(
-    @Param('clubId') clubId: number,
+    @Param('clubId', new ParseIntPipe()) clubId: number,
     @Query('date') date: Date,
   ) {
     return await this.studioSlotsService.getSlotsForClub(clubId, date);
@@ -52,13 +37,17 @@ export class StudioSlotsController {
 
   @ApiOkResponse({ type: [GetClubSlotRdo] })
   @Get('studios/:studioId/slots')
-  async findAllForStudio(@Param('studioId') studioId: number) {
+  async findAllForStudio(
+    @Param('studioId', new ParseIntPipe()) studioId: number,
+  ) {
     return await this.studioSlotsService.getSlotsForStudio(studioId);
   }
 
   @ApiOkResponse({ type: [GetSlotsForStudio] })
   @Get('studios/:studioId/clubs/slots')
-  async findAllForClubsOfStudios(@Param('studioId') studioId: number) {
+  async findAllForClubsOfStudios(
+    @Param('studioId', new ParseIntPipe()) studioId: number,
+  ) {
     return await this.studioSlotsService.getSlotsForClubsOfStudio(
       studioId,
       new Date(),
@@ -75,41 +64,7 @@ export class StudioSlotsController {
 
   @ApiOkResponse({ type: GetTimeTableRdo })
   @Get('studios/:studioId/timetable')
-  async getTimeTable(@Param('studioId') studioId: number) {
+  async getTimeTable(@Param('studioId', new ParseIntPipe()) studioId: number) {
     return await this.studioSlotsService.getStudioTimeTable(14, studioId);
-  }
-
-  // @ApiOkResponse({ type: GetClubSlotRdo })
-  // @Get('clubs/:clubId/slots/:id')
-  // async findOne(@Param('id') id: number) {
-  //   return await this.studioSlotsService.findOne({
-  //     where: { id },
-  //     relations: { studio: { city: true } },
-  //   });
-  // }
-
-  // @ApiOkResponse({ type: GetClubSlotRdo })
-  // @ApiBody({ type: UpdateClubSlotDto })
-  // @AuthGuard()
-  // @Patch('clubs/:clubId/slots/:id')
-  // async update(
-  //   @Param('id') id: number,
-  //   @Body() updateSlotDto: UpdateClubSlotDto,
-  // ) {
-  //   return await this.studioSlotsService.updateOne(
-  //     {
-  //       where: { id },
-  //       relations: { studio: { city: true, studio: true } },
-  //     },
-  //     {
-  //       ...updateSlotDto,
-  //       studio: { id: updateSlotDto.club },
-  //     },
-  //   );
-  // }
-
-  @Delete('clubs/:clubId/slots/:id')
-  async remove(@Param('id') id: number) {
-    return await this.studioSlotsService.remove({ where: { id } });
   }
 }
