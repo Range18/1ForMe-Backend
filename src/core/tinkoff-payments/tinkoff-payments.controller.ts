844e83403e0c36bd0862168b5090568822c5396b
Range18 <shakmate@mail.ru>
2025-01-23 22:08:23 +0500

    Edit http code in tinkoff notification handle

--- src/core/tinkoff-payments/tinkoff-payments.controller.ts
+++ src/core/tinkoff-payments/tinkoff-payments.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Ip, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Ip, Post } from '@nestjs/common';
 import { TinkoffPaymentsService } from '#src/core/tinkoff-payments/tinkoff-payments.service';
 import { PaymentNotificationDto } from '#src/core/tinkoff-payments/dto/payment-notification.dto';
 
@@ -8,6 +8,7 @@ export class TinkoffPaymentsController {
     private readonly tinkoffPaymentsService: TinkoffPaymentsService,
   ) {}
 
+  @HttpCode(200)
   @Post('notification')
   async handleNotification(
     @Ip() ip: string,
